895e87c779cc516b0bda49a733f799f39b2db17e
Asanilta Fahda <asanilta@gmail.com>
2015-04-24 10:49:18 +0700

    Product & Player

--- src/Soap.java
+++ src/Soap.java
@@ -4,32 +4,39 @@
  * and open the template in the editor.
  */
 
-
 /**
  *
  * @author ASUS X202E
  */
 public class Soap extends Product {
     public Soap() {
-        name = "Small Soap";
-        description = "Increases hygiene by 10 points";
-        upgradePrice = 100;
-        index = 0.1;
-        level = 1;
+        this.name = "Small Soap";
+        this.description = "Increases hygiene by 10 points";
+        this.upgradePrice = 100;
+        this.index = 0.1;
+        this.level = 1;
     }
 
     /**
      *
+     * @param lvl
      */
     @Override
-    public void upgrade() {
-        if (getLevel()==1) {
+    public void changeLevel(int lvl) {
+        /*if (lvl==1) {
+            setName("Small Soap");
+            setDescription("Increases hygiene by 10 points");
+            setUpgradePrice(100);
+            setIndex(0.1);
+            setLevel(1);
+        }*/
+        if (lvl==2) {
             setName("Medium Soap");
             setDescription("Increases hygiene by 20 points");
             setUpgradePrice(200);
             setIndex(0.2);
             setLevel(2);
-        } else if (getLevel()==2) {
+        } else if (lvl==3) {
             setName("Large Soap");
             setDescription("Increases hygiene by 30 points");
             setUpgradePrice(99999999);
