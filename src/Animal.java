375e26206b217826b0e2ffa573e0cb63534620e2
tifaniwarnita <tiffayumuyuka@gmail.com>
2015-04-24 10:07:50 +0700

    Implements Subject

--- src/Animal.java
+++ src/Animal.java
@@ -1,3 +1,8 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observer;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,15 +13,18 @@
  *
  * @author Tifani
  */
-public class Animal {
+public class Animal implements Subject{
     private String name;
     private int hunger;
     private int happiness;
     private int health;
     private int hygiene;
     private int state; //bangun = 0, tidur = 1
-    //private View AnimalView;
+    private List<Observer> observers;
     private String type;
+    private final Object MUTEX = new Object();
+    private boolean changed;
+    private String message; //TODO
     
     /**
      *
@@ -32,6 +40,8 @@ public class Animal {
         this.state = 0;
         //Animal View
         this.type = category;
+        this.observers = new ArrayList<>();
+        this.changed = false; //TODO
     }
 
     /**
@@ -134,45 +144,80 @@ public class Animal {
     
     public void performMovement() {
         //TODO
+        this.changed = true;
     }
     
     public void eatFood(Food f) {
         this.setHunger((int) (this.getHunger()+f.getIndex()*100));
         if (this.getHunger()>100)
             this.setHunger(100);
+        this.changed = true;
     }
     
     public void cleanBody(Soap s) {
         this.setHygiene((int) (this.getHygiene()+s.getIndex()*100));
         if (this.getHygiene()>100)
             this.setHygiene(100);
+        this.changed = true;
     }
     
-    public void consumePotion() {
-        //TODO (removed?)
+    public void playGame(Toy t) {
+        this.setHappiness(this.getHappiness()+t.getIndex()*100);
+        if (this.getHappiness()>100)
+            this.setHappiness(100);
+        this.changed = true;
     }
     
     public void sleep() {
         this.state = 1;
+        this.changed = true;
     }
     
     public void wakeUp() {
         this.state = 0;
+        this.changed = true;
     }
     
     public void reduceAllStatus() {
         //TODO
     }
-    
+
+    @Override
+    public void registerObserver(Observer obj) {
+        if (obj==null)
+            throw new NullPointerException("Null Observer");
+        synchronized (MUTEX) {
+            if(!observers.contains(obj))
+                observers.add(obj);
+        }
+    }
+
+    @Override
+    public void unregisterObserver(Observer obj) {
+        synchronized (MUTEX) {
+            observers.remove(obj);
+        }
+    }
+
+    @Override
     public void notifyObserver() {
-        //TODO
+        List<Observer> observersLocal = null;
+        //sinkronisasi digunakan untuk memastikan bahwa setiap observer yang terregister setelah pesan diterima tidak ternotify
+        synchronized (MUTEX) {
+            if(!changed)
+                return;
+            observersLocal = new ArrayList<>(this.observers);
+            this.changed = false;
+        }
+        for (Observer obj : observersLocal) {
+            // TODO obj.notify();
+        }
+    }
+
+    @Override
+    public Object getUpdate(Observer obj) {
+        return this.message;
     }
     
-    public void registerObserver() {
-        //TODO
-    }
     
-    public void unregisterObserver() {
-        //TODO
-    }
 }
